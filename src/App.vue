2a6796c669bfdd3215e3997f2b2eaa63263c8f5a
David Gray <grayda@solidinc.org>
2021-06-16 14:47:58 +1000

    Import and add getBatteryInfo call

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,18 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
+import { Device } from '@capacitor/device'
+
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  mounted() {
+    Device.getBatteryInfo().then((info) => {
+      alert("Battery info: " + JSON.stringify(info))
+    })
   }
 });
 </script>
